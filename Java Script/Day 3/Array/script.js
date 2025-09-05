5f1634e5debe011399d507919bd3e3dc7d3f6088
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-05 19:53:02 +0530

    Basic Function Over

---
+++ Java Script/Day 3/Array/script.js
@@ -0,0 +1,3 @@
+let arr = ["VP", 2004, "PV"]
+
+console.log(arr)
--- Java Script/Day 3/Functions/script.js
+++ Java Script/Day 3/Functions/script.js
@@ -1,8 +1,35 @@
-function Add(v,p){
-    console.log("V :",v)
-    console.log("P :",p)
-    console.log("V + P :",v+p)
-}
 
-Add(1,3)
-Add(4,8)
+function Add(v, p) {
+    console.log("V + P :", v + p)
+}
+console.log("Without Return")
+Add(1, 3)
+Add(4, 8)
+
+
+function Pow(v, p) {
+    return v ** p
+}
+console.log("\nWith Return")
+console.log("Power :", Pow(4, 8))
+
+
+function UserId() {
+    return Math.floor(Math.random() * 10001)
+}
+console.log("\nUserId Using Random :", UserId())
+
+
+function ReturnObject() {
+    return {
+        name: "VP",
+        age: 20
+    }
+}
+console.log("\nObject Function :\n", ReturnObject())
+
+
+function ReturnArray() {
+    return ["VP", 2004, "PV"]
+}
+console.log("\nArray Function :\n", ReturnArray())
