2c0293eda638ea5bb9b5e03b64aafc602beb66d0
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-05 20:05:54 +0530

    Basic Array Learning

--- Java Script/Day 3/Array/script.js
+++ Java Script/Day 3/Array/script.js
@@ -1,3 +1,13 @@
 let arr = ["VP", 2004, "PV"]
 
-console.log(arr)
+function PrintArr() {
+    console.log("Array")
+    console.log(arr)
+}
+
+function PrintArrData() {
+    console.log("Array Data")
+    for (i in arr) {
+        console.log(arr[i])
+    }
+}
