138081cdc17c67e5222da4ebd048201458ace1d3
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-06 14:33:56 +0530

    Array & Loop Practice

--- Java Script/Day 3/Array/script.js
+++ Java Script/Day 3/Array/script.js
@@ -6,8 +6,13 @@ function PrintArr() {
 }
 
 function PrintArrData() {
-    console.log("Array Data")
+    console.log("Array Data Using (for in)")
     for (i in arr) {
         console.log(arr[i])
+    }
+
+    console.log("Array Data Using (for of)")
+    for (i of arr) {
+        console.log(i)
     }
 }
---
+++ Java Script/Day 3/Loops/For_Loop/script.js
@@ -0,0 +1,11 @@
+let v = 8
+
+for (var i = 0; i <= v; i++) {
+    console.log(i)
+}
+
+let arr = ["VP",2004,"PV"]
+
+for(i of arr){
+    console.log(i)
+}
