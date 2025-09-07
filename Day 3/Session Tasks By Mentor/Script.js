fa81500d7f1e21e151468c3b8d8c8289dcf277a9
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-07 18:39:32 +0530

    Session Task

---
+++ Day 3/Session Tasks By Mentor/Script.js
@@ -0,0 +1,22 @@
+// 1. print the numbers from 1 to 100 with the interval of 10
+console.log("1. print the numbers from 1 to 100 with the interval of 10");
+for (var i = 0; i <= 100; i += 10) {
+  console.log(i);
+}
+
+// 2. print the number from 10 to 1
+console.log("2. print the number from 10 to 1");
+for (var j = 10; j >= 1; j -= 1) {
+  console.log(j);
+}
+
+// 3. print the multiple of 2 until 10
+console.log("3. print the multiple of 2 until 10");
+
+for (var k = 1; k <= 10; k++) {
+  console.log(k * 2);
+}
+
+// 4. print the number from 100 to 1 with the difference of 10.
+console.log("4. print the number from 100 to 1 with the difference of 10.");
+for(let l=100;)
