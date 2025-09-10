b801ee5ddac007355697d3a040547abb04efca6b
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-10 22:41:39 +0530

    Object & Object Methods Practice

--- Day 4/Objects_JSON/script.js
+++ Day 4/Objects_JSON/script.js
@@ -1,1 +1,12 @@
-console.log("VP")
+// Object
+
+console.log("\nObject :")
+
+let vp = {
+    name: "VP",
+    age: 21,
+}
+
+console.log("Keys : ", Object.keys(vp))
+console.log("Values : ", Object.values(vp))
+console.log("Full Object :", vp)
