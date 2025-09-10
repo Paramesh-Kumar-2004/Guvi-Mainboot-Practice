177da0498196c48bfc9d8cf8c49ca2dc292bf654
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-10 22:26:03 +0530

    Scope Practice

--- Day 4/Hoisting_Scope/script.js
+++ Day 4/Hoisting_Scope/script.js
@@ -1,8 +1,29 @@
 // Hoisting
 
+console.log("Hoisting")
 console.log(v) // op : undefined (When I Use var It Show undefined But Rally This is A Error Here The Hoisting Entered)
 var v = "VP"
 
-
-console.log(p) // Error
+// console.log(p) // Error
 let p = "V"
+
+
+
+// Scope
+/*
+1. Local Scope
+2. Global Scope
+*/
+
+
+console.log("\nScope")
+let vp = 2004; // Global 
+
+if (true) {
+    let pv = "PV"
+    console.log("Inside Of The Condition vp :", vp)
+    console.log("Inside Of The Condition pv :", pv)
+}
+
+console.log("Outside Of The Condition vp :", vp)
+
