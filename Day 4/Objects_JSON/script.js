ee87bc9b4a2faaffd7fa9eeea9b9f9b67684c283
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-09-10 22:43:35 +0530

    Object & Object Methods Practice

--- Day 4/Objects_JSON/script.js
+++ Day 4/Objects_JSON/script.js
@@ -10,3 +10,16 @@ let vp = {
 console.log("Keys : ", Object.keys(vp))
 console.log("Values : ", Object.values(vp))
 console.log("Full Object :", vp)
+
+
+// JSON
+console.log("\nJSON :")
+
+let vp2 = {
+    "name": "VP",
+    "age": "21"
+}
+
+console.log("Json Keys : ", Object.keys(vp2))
+console.log("Json Values : ", Object.values(vp2))
+console.log("Full Json :", vp2)
